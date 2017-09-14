02ded153ea5890b141d09a56f2e53b6a7e3c89ef
Michael Joyce <ubermichael@gmail.com>
2017-09-14 13:06:17 -0700

    clean up the import a whole bunch.

--- src/AppBundle/Command/ImportCsvCommand.php
+++ src/AppBundle/Command/ImportCsvCommand.php
@@ -14,6 +14,7 @@ use AppBundle\Entity\Work;
 use AppBundle\Entity\WorkCategory;
 use Doctrine\Common\Persistence\ObjectManager;
 use Exception;
+use Monolog\Logger;
 use Nines\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
@@ -30,15 +31,23 @@ class ImportCsvCommand extends ContainerAwareCommand {
     private $em;
     
     /**
+     *
+     * @var Logger
+     */
+    private $logger;
+    
+    /**
      * If true, commit the import to the database. Set by the commit option
      * at the command line.
      * 
      * @var boolean
      */
     private $commit;
+    
     private $from;
-    private $to;
-
+    
+    private $lineNumber;
+    
     public function __construct($name = null) {
         parent::__construct($name);
         $this->commit = false;
@@ -50,12 +59,12 @@ class ImportCsvCommand extends ContainerAwareCommand {
         $this->addArgument('file', InputArgument::REQUIRED, 'File to import.');
         $this->addOption('commit', null, InputOption::VALUE_NONE, 'Commit the import to the database.');
         $this->addOption('from', null, InputOption::VALUE_REQUIRED, 'Start import at this row.');
-        $this->addOption('to', null, InputOption::VALUE_REQUIRED, 'Stop importing at this row.');
     }
 
     public function setContainer(ContainerInterface $container = null) {
         parent::setContainer($container);
         $this->em = $container->get('doctrine')->getManager();
+        $this->logger = $container->get('logger');
     }
 
     protected function headersToIndex($row) {
@@ -81,12 +90,17 @@ class ImportCsvCommand extends ContainerAwareCommand {
 
     protected function setWorkCategory($work, $column = null) {
         if (!$column) {
-            $column = 'book';
+            $this->logger->error("Missing work category on line {$this->lineNumber}: {$column}");
+            $column = 'Book';
+        }
+        if($column == 'MS') {
+            $column = 'Manuscript';
         }
         $repo = $this->em->getRepository(WorkCategory::class);
-        $category = $repo->findOneBy(array('name' => $column));
+        $category = $repo->findOneBy(array('label' => $column));
         if (!$category) {
-            throw new Exception("Uknown category " . $column);
+            $this->logger->error("Unknown work category on line {$this->lineNumber}: {$column}");
+            $category = $repo->findOneBy(array('label' => 'Book'));
         }
         $work->setWorkCategory($category);
         $category->addWork($work);
@@ -139,10 +153,16 @@ class ImportCsvCommand extends ContainerAwareCommand {
         $repo = $this->em->getRepository(DateCategory::class);
         $dateCategory = $repo->findOneBy(array('label' => $dateTypeCol));
         if (!$dateCategory) {
-            throw new Exception('Uknown date type ' . $dateTypeCol);
+            $this->logger->error("Malformed date type line:{$this->lineNumber}. '{$dateTypeCol}'");
+            return;
         }
         $date = new DateYear();
+        try {
         $date->setValue($dateCol);
+        } catch (\Exception $e) {
+            $this->logger->error("Malformed date line:{$this->lineNumber}. '{$dateCol}'");
+            return;
+        }
         $date->setDateCategory($dateCategory);
         $date->setWork($work);
         $work->addDate($date);
@@ -175,7 +195,8 @@ class ImportCsvCommand extends ContainerAwareCommand {
                 'label' => $label
             ));
             if (!$subject) {
-                throw new Exception('Unknown subject ' . $label);
+                $this->logger->error("Unknown subject on line {$this->lineNumber}. '{$label}'");
+                return;
             }
             $work->addSubject($subject);
             $subject->addWork($work);
@@ -189,26 +210,11 @@ class ImportCsvCommand extends ContainerAwareCommand {
         $repo = $this->em->getRepository(Genre::class);
         $genre = $repo->findOneBy(array('label' => $label));
         if (!$genre) {
-            throw new Exception('Unknown genre ' . $label);
+            $this->logger->error("Unknown genre on line {$this->lineNumber}. '{$label}'");
+            return;
         }
         $work->setGenre($genre);
         $genre->addWork($work);
-    }
-
-    protected function setCreatedBy($work, $name) {
-        $repo = $this->em->getRepository(User::class);
-        $user = $repo->findOneBy(array(
-            'fullname' => $name
-        ));
-        if (!$user) {
-            $user = new User();
-            $user->setEmail($name . '@example.com');
-            $user->setFullname($name);
-            $user->setInstitution('SFU');
-            $user->setPlainPassword(md5(uniqid()));
-            $this->persist($user);
-        }
-        $work->setCreatedBy($user);
     }
 
     protected function addCheckedBy($work, $name) {
@@ -231,67 +237,116 @@ class ImportCsvCommand extends ContainerAwareCommand {
         $matches = array();
         $work = new Work();
         $work->setTitle($row[0]);
-        $this->setWorkCategory($work);
+        
         if ($row[2] && preg_match('/(\d+)/', $row[2], $matches)) {
             $work->setVolume($matches[1]);
+        } elseif($row[2]) {
+            $this->logger->error("Malformed volume line:{$this->lineNumber}, col:2. '{$row[2]}'");
         }
+        
         $this->importContribution($work, $row[3], $row[4]);
         $this->importContribution($work, $row[5], $row[6]);
         $this->importContribution($work, $row[7], $row[8]);
-
+        
         $this->importDate($work, $row[9], $row[10]);
         if ($row[11] && preg_match('/(\d+)/', $row[11], $matches)) {
             $work->setEdition($matches[1]);
+        } elseif($row[11]) {
+            $this->logger->error("Malformed edition line:{$this->lineNumber}, col:11. '{$row[11]}'");
         }
+        
         if ($row[12]) {
             $work->setPublicationPlace($row[12]);
         }
+        
         $this->importPublisher($work, $row[13]);
+        
         if ($row[14]) {
             $work->setPhysicalDescription($row[14]);
         }
-        if ($row[15]) {
-            $work->setIllustrations('Yes' === $row[15]);
+        
+        switch($row[15]) {
+            case '': break;
+            case 'Yes': $work->setIllustrations(true); break;
+            case 'No': $work->setIllustrations(false); break;
+            default: $this->logger->error("Unexpected value line {$this->lineNumber} col 15: '{$row[15]}'");
         }
-        if ($row[16]) {
-            $work->setFrontispiece('Yes' === $row[16]);
+
+        switch($row[16]) {
+            case '': break;
+            case 'Yes': $work->setFrontispiece(true); break;
+            case 'No': $work->setFrontispiece(false); break;
+            default: $this->logger->error("Unexpected value line {$this->lineNumber} col 16: '{$row[16]}'");
         }
+
         if ($row[17]) {
             $work->setTranslationDescription($row[17]);
         }
+        
         if ($row[18]) {
             $work->setDedication($row[18]);
         }
         // 20 is author of preface (skipped)
         // 21 is worldcat subject (skipped)
 
-        if ($row[21]) {
-            $work->setWorldcatUrl($row[21]);
+        if ($row[21] && filter_var(trim($row[21]), FILTER_VALIDATE_URL)) {
+            $work->setWorldcatUrl(trim($row[21]));
+        } elseif($row[21]) {
+            $this->logger->error("Invalid URL on line {$this->lineNumber} col 21: '{$row[21]}'");
         }
 
         $this->addSubjects($work, array($row[22], $row[23], $row[24]));
         $this->setGenre($work, $row[25]);
-        if ($row[26]) {
-            $work->setTranscription($row[26] === 'Yes');
+        
+        switch($row[26]) {
+            case '': break;
+            case 'Yes': $work->setTranscription(true); break;
+            case 'No': $work->setTranscription(false); break;
+            default: $this->logger->error("Unexpected value line {$this->lineNumber} col 26: '{$row[26]}'");
         }
+
+        
         $work->setPhysicalLocations($row[27]);
         $work->setDigitalLocations($row[28]);
-        if ($row[29]) {
-            $work->setDigitalUrl($row[29]);
+        
+        if ($row[29] && filter_var(trim($row[29]), FILTER_VALIDATE_URL)) {
+            $work->setDigitalUrl(trim($row[29]));
+        } elseif($row[29]) {
+            $this->logger->error("Invalid URL on line {$this->lineNumber} col 29: '{$row[29]}'");
         }
+        
         if ($row[30]) {
             $work->setNotes($row[30]);
         }
+        
         if ($row[31]) {
-            $this->setCreatedBy($work, $row[31]);
+            foreach(preg_split("/\s?[\/,]\s*/", $row[31]) as $checkedBy) {
+                $checkedBy = preg_replace("/^\s|\s$/", '', $checkedBy);
+                $checkedBy = strtoupper($checkedBy);
+                $checkedBy = preg_replace("/[^A-Z]/", '', $checkedBy);
+                if( ! preg_match("/^[A-Z]+$/", $checkedBy)) {
+                    $this->logger->error("Invalid initials on line {$this->lineNumber} col 31: '{$checkedBy}'");
+                    continue;
+                }
+                $this->addCheckedBy($work, $checkedBy);
+            }
         }
 
         $this->importContribution($work, $row[32], $row[33]);
         $this->importContribution($work, $row[34], $row[35]);
         $this->importContribution($work, $row[36], $row[37]);
-
-        if ($row[38]) {
-            $this->addCheckedBy($work, $row[38]);
+        
+        $this->setWorkCategory($work, $row[38]);
+        
+        if($row[39]) {
+            $work->setEditorialNotes($row[39]);
+        }
+        
+        switch(strtolower(trim($row[40]))) {
+            case '': break;
+            case 'yes': $work->setComplete(true); break;
+            case 'no': $work->setComplete(false); break;
+            default: $this->logger->error("Unexpected value on line {$this->lineNumber} col 40: '$row[40]'");
         }
 
         $this->persist($work);
@@ -300,19 +355,16 @@ class ImportCsvCommand extends ContainerAwareCommand {
 
     protected function import($path, OutputInterface $output) {
         $fh = fopen($path, 'r');
-        $n = 0;
+        $this->lineNumber = 1;
         $headers = fgetcsv($fh);
-        $n++;
+        $this->lineNumber++;
         $index = $this->headersToIndex($headers);
         while (($row = fgetcsv($fh))) {
-            $n++;
-            if($this->from && $n < $this->from) {
+            $this->lineNumber++;
+            if($this->from && $this->lineNumber < $this->from) {
                 continue;
             }
-            if($this->to && $this->to < $n) {
-                continue;
-            }
-            $output->writeln( $n . ': ' . $row[0]);
+            $this->logger->notice($this->lineNumber . ': ' . $row[0]);
             $this->importRow($row, $index);
         }
     }
@@ -323,13 +375,13 @@ class ImportCsvCommand extends ContainerAwareCommand {
             $this->commit = true;
         }
         if ($input->getOption('from')) {
-            $this->from = $input->getOption('from');
+            $this->from = $input->getOption('from') + 1;
         }
-        if ($input->getOption('to')) {
-            $this->to = $input->getOption('to');
+        try {
+            $this->import($file, $output);
+        } catch(\Exception $e) {
+            $this->logger->critical($e->getMessage() . " in CSV file after line {$this->lineNumber}");
         }
-        $output->writeln($file);
-        $this->import($file, $output);
     }
 
 }
