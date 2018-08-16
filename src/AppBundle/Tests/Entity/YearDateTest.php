a4dbacca6a269b74aff9c62e496e4b9770743512
Michael Joyce <ubermichael@gmail.com>
2018-08-16 12:45:25 -0700

    Fix some missing tests.

--- src/AppBundle/Tests/Entity/YearDateTest.php
+++ src/AppBundle/Tests/Entity/YearDateTest.php
@@ -3,10 +3,10 @@
 namespace AppBundle\Tests\Entity;
 
 use AppBundle\Entity\DateYear;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-class YearDateTest extends PHPUnit_Framework_TestCase {
-    
+class YearDateTest extends TestCase {
+
     /**
      * @dataProvider setValueData
      */
@@ -15,40 +15,40 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->assertEquals($expected, $date->getValue());
     }
-    
+
     public function setValueData() {
         return array(
             ["1800", "1800"],
             ["c1800", "c1800"],
             ["c1800", "C1800"],
             ["c1800", " c 1800 "],
-            
+
             ["1800-", "1800-"],
             ["1800-", "1800 - "],
             ["c1800-", "c1800-"],
             ["c1800-", "C1800-"],
             ["c1800-", " c 1800 - "],
-            
+
             ["-1800", "-1800"],
             ["-1800", " - 1800"],
             ["-c1800", "-c1800"],
             ["-c1800", "-C1800"],
             ["-c1800", " - c1800"],
-            
+
             ["1800-1801", "1800-1801"],
             ["c1800-1801", "c1800-1801"],
             ["1800-c1801", "1800-c1801"],
             ["c1800-c1801", "c1800-c1801"],
             ["c1800-c1801", "C1800-C1801"],
-            
+
             ["1800-1801", " 1800 - 1801 "],
             ["c1800-1801", " c 1800 - 1801 "],
             ["1800-c1801", " 1800 - c 1801 "],
             ["c1800-c1801", " c 1800 - c 1801 "],
-            ["c1800-c1801", " C 1800 - C 1801 "],            
+            ["c1800-c1801", " C 1800 - C 1801 "],
         );
     }
-    
+
     /**
      * @dataProvider setBadValueData
      * @expectedException Exception
@@ -58,13 +58,13 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->fail("Set value did not throw an exception.");
     }
-    
+
     public function setBadValueData() {
         return array (
             [null],
             [false],
             [true],
-            ['cheese'],            
+            ['cheese'],
             ['180'],
             ['c180'],
             ['-180'],
@@ -74,7 +74,7 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
             ['x1989'],
         );
     }
-    
+
     /**
      * @dataProvider rangeData
      */
@@ -83,23 +83,23 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->assertEquals($expected, $date->isRange());
     }
-    
+
     public function rangeData() {
         return array(
             [false, 1800],
             [false, "1800"],
             [false, "c1800"],
             [false, "C1799"],
-            
+
             [true, -1800],
             [true, "-1800"],
             [true, "-c1800"],
             [true, "-C1800"],
-            
+
             [true, "1800-"],
             [true, "c1800-"],
             [true, "C1800-"],
-            
+
             [true, "1800-1805"],
             [true, "c1800-1805"],
             [true, "1800-c1805"],
@@ -107,10 +107,10 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
             [true, "1800-1805"],
             [true, "C1800-1805"],
             [true, "1800-C1805"],
-            [true, "C1800-C1805"],            
+            [true, "C1800-C1805"],
         );
     }
-    
+
     /**
      * @dataProvider hasStartData
      */
@@ -119,23 +119,23 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->assertEquals($expected, $date->hasStart());
     }
-    
+
     public function hasStartData() {
         return array(
             [true, 1800],
             [true, "1800"],
             [true, "c1800"],
             [true, "C1799"],
-            
+
             [false, -1800],
             [false, "-1800"],
             [false, "-c1800"],
             [false, "-C1800"],
-            
+
             [true, "1800-"],
             [true, "c1800-"],
             [true, "C1800-"],
-            
+
             [true, "1800-1805"],
             [true, "c1800-1805"],
             [true, "1800-c1805"],
@@ -155,23 +155,23 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->assertEquals($expected, $date->getStart());
     }
-    
+
     public function getStartData() {
         return array(
             ["1800", 1800],
             ["1800", "1800"],
-            ['c1800', "c1800"],            
+            ['c1800', "c1800"],
             ['c1799', "C1799"],
-            
+
             [null, -1800],
             [null, "-1800"],
             [null, "-c1800"],
             [null, "-C1800"],
-            
+
             ["1800", "1800-"],
             ["c1800", "c1800-"],
             ["c1800", "C1800-"],
-            
+
             ["1800", "1800-1805"],
             ["c1800", "c1800-1805"],
             ["1800", "1800-c1805"],
@@ -182,7 +182,7 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
             ["c1800", "C1800-C1805"],
         );
     }
-    
+
     /**
      * @dataProvider hasEndData
      */
@@ -191,23 +191,23 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
         $date->setValue($value);
         $this->assertEquals($expected, $date->hasEnd());
     }
-    
+
     public function hasEndData() {
         return array(
             [true, 1800],
             [true, "1800"],
             [true, "c1800"],
             [true, "C1799"],
-            
+
             [true, -1800],
             [true, "-1800"],
             [true, "-c1800"],
             [true, "-C1800"],
-            
+
             [false, "1800-"],
             [false, "c1800-"],
             [false, "C1800-"],
-            
+
             [true, "1800-1805"],
             [true, "c1800-1805"],
             [true, "1800-c1805"],
@@ -225,34 +225,34 @@ class YearDateTest extends PHPUnit_Framework_TestCase {
     public function testGetEnd($expected, $value) {
         $date = new DateYear();
         $date->setValue($value);
-        //$this->assertEquals($expected, $date->getEnd());
+        $this->assertEquals($expected, $date->getEnd());
     }
-    
+
     public function getEndData() {
         return array(
             ["1800", 1800],
             ["1800", "1800"],
-            ['c1800', "c1800"],            
+            ['c1800', "c1800"],
             ['c1799', "C1799"],
-            
-            [null, -1800],
-            [null, "-1800"],
-            [null, "-c1800"],
-            [null, "-C1800"],
-            
-            ["1800", "1800-"],
-            ["c1800", "c1800-"],
-            ["c1800", "C1800-"],
-            
-            ["1800", "1800-1805"],
-            ["c1800", "c1800-1805"],
-            ["1800", "1800-c1805"],
-            ["c1800", "c1800-c1805"],
-            ["1800", "1800-1805"],
-            ["c1800", "C1800-1805"],
-            ["1800", "1800-C1805"],
-            ["c1800", "C1800-C1805"],
+
+            ["1800", -1800],
+            ["1800", "-1800"],
+            ["c1800", "-c1800"],
+            ["c1800", "-C1800"],
+
+            [null, "1800-"],
+            [null, "c1800-"],
+            [null, "C1800-"],
+
+            ["1805", "1800-1805"],
+            ["1805", "c1800-1805"],
+            ["c1805", "1800-c1805"],
+            ["c1805", "c1800-c1805"],
+            ["1805", "1800-1805"],
+            ["1805", "C1800-1805"],
+            ["c1805", "1800-C1805"],
+            ["c1805", "C1800-C1805"],
         );
     }
-    
+
 }
