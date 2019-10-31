e2de479671015c9a91dadb8b6ac35eadc1985c19
Michael Joyce <ubermichael@gmail.com>
2019-10-31 12:59:41 -0700

    github issue templates, submodules and config.

--- sami.php
+++ sami.php
@@ -1,32 +1,23 @@
 <?php
 
-//require 'vendor/autoload.php';
-
 use Sami\Sami;
 use Sami\RemoteRepository\GitHubRemoteRepository;
 use Symfony\Component\Finder\Finder;
 
 $dir = __DIR__;
 
-$iterator = Finder::create()
-          ->files()
-          ->name('*.php')
-          ->exclude('Resources')
-          ->exclude('Tests')
-          ->in($dir . '/src/AppBundle');
-
-$options = array(
-    // 'theme' => 'symfony',
-    'title' => 'NEWN Internals',
-    'build_dir' => $dir . '/web/docs/sami',
-    'cache_dir' => $dir . '/var/cache/sami',
+$config = array(
+//    'theme'                => 'symfony',
+    'title'                => 'Circus Internal API',
+    'build_dir'            => $dir . '/web/docs/api',
+    'cache_dir'            => $dir . '/var/cache/sami',
+    'remote_repository'    => new GitHubRemoteRepository('ubermichael/circus', $dir),
     'default_opened_level' => 2,
-    'include_parent_data' => true,
-    'insert_todos' => true,
-    'sort_class_properties' => true,
-    'sort_class_methods' => true,
-    'sort_class_traits' => true,
-    'sort_class_interfaces' => true,
 );
 
-return new Sami($iterator, $options);
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('src');
+
+return new Sami($iterator, $config);
