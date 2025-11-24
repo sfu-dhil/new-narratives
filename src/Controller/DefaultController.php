bda35231138e3b2a4538d8df569a6528f7bb0930
Andrew Gardener <andrew.e.gardener@gmail.com>
2025-11-24 14:09:43 -0800

    Update privacy policy page
    Use common dhil docs privacy page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -21,8 +21,4 @@ class DefaultController extends AbstractController implements PaginatorAwareInte
             'homepage' => $repo->findHomepage(),
         ];
     }
-
-    #[Route(path: '/privacy', name: 'privacy')]
-    #[Template]
-    public function privacy() : void {}
 }
